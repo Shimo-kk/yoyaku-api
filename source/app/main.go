0d4328557ae6a3c26e1fa30cfa006810d6fdcc81
Shimo-kk <kk.shimozuma@gmail.com>
2024-02-28 17:03:56 +0900

    setup:ロガー設定
    ・ロガー設定ファイル追加
    ・ロガー設定処理追加

--- source/app/main.go
+++ source/app/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"path/filepath"
 	"yoyaku/app/infrastructure/database/postgres"
 	"yoyaku/app/presentation/router"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
@@ -15,7 +17,33 @@ func main() {
 
 	// 環境変数の読み込み
 	if err := godotenv.Load(); err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+
+	// ロガー設定の読み込み
+	configYaml, err := os.ReadFile("./config/logger.yaml")
+	if err != nil {
+		panic(err)
+	}
+	var loggerConfig zap.Config
+	if err := yaml.Unmarshal(configYaml, &loggerConfig); err != nil {
+		panic(err)
+	}
+
+	// ログディレクトリの作成
+	logDir := os.Getenv("LOG_OUTPUT_DIR")
+	if _, err := os.Stat(logDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(logDir, 0755); err != nil {
+			panic(err)
+		}
+	}
+
+	// ロガーの生成
+	loggerConfig.OutputPaths = append(loggerConfig.OutputPaths, filepath.Join(logDir, "app.out.log"))
+	loggerConfig.ErrorOutputPaths = append(loggerConfig.ErrorOutputPaths, filepath.Join(logDir, "app.err.log"))
+	logger, err := loggerConfig.Build()
+	if err != nil {
+		panic(err)
 	}
 
 	// DB初期化
@@ -23,7 +51,7 @@ func main() {
 	defer postgres.CloseDB(db)
 
 	// ルーターの設定
-	router.IncludeRouter(e, db)
+	router.IncludeRouter(e, db, logger)
 
 	// サーバーの起動
 	port := os.Getenv("MY_PORT")
--- source/app/presentation/router/router.go
+++ source/app/presentation/router/router.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
-func IncludeRouter(e *echo.Echo, db *gorm.DB) {
+func IncludeRouter(e *echo.Echo, db *gorm.DB, logger *zap.Logger) {
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Hello, world!")
 	})
